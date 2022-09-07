4bdaf394ce6a3ebaf6b2d112d07e7750fc34484b
Cédric Derue <cedric.derue@gmail.com>
2022-09-07 15:20:14 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,19 +1,35 @@
 terraform {
 
-  cloud {
-    organization = "hashicorp-learn"
+  /*cloud {
+    organization = "altran"
 
     workspaces {
-      name = "learn-terraform-cloud"
+      name = "learn-terraform-cloud-azure"
     }
-  }
-
+  }*/
+  
+# 2. Specify the version of the AzureRM Provider to use
+terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=3.0.1"
     }
   }
+}
+
+# 3. Configure the AzureRM Provider
+provider "azurerm" {
+  # The AzureRM Provider supports authenticating using via the Azure CLI, a Managed Identity
+  # and a Service Principal. More information on the authentication methods supported by
+  # the AzureRM Provider can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs#authenticating-to-azure
+
+  # The features block allows changing the behaviour of the Azure Provider, more
+  # information can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/features-block
+  features {}
+}
 
   required_version = ">= 0.14.0"
 }
